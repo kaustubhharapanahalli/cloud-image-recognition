f59d375c91d2500c4f9e73a613941065e52de1dc
Kaustubh Harapanahalli <kaustubhharapanahalli@gmail.com>
2023-10-01 14:58:12 -0700

    Convert the script into a callable function

--- image_recognition/image_classification.py
+++ image_recognition/image_classification.py
@@ -1,31 +1,40 @@
+import json
+import os
+import sys
+
+import numpy as np
 import torch
-import torchvision
-import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
-from urllib.request import urlopen
+import torchvision.transforms as transforms
 from PIL import Image
-import numpy as np
-import json
-import sys
-import time
 
-url = str(sys.argv[1])
-#img = Image.open(urlopen(url))
-img = Image.open(url)
 
-model = models.resnet18(pretrained=True)
+def image_recognition(
+    image_location: str, parent_folder: str = "images"
+) -> str:
+    img = Image.open(os.path.join(parent_folder, image_location))
 
-model.eval()
-img_tensor = transforms.ToTensor()(img).unsqueeze_(0)
-outputs = model(img_tensor)
-_, predicted = torch.max(outputs.data, 1)
+    model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 
-with open('./imagenet-labels.json') as f:
-    labels = json.load(f)
-result = labels[np.array(predicted)[0]]
-img_name = url.split("/")[-1]
-#save_name = f"({img_name}, {result})"
-save_name = f"{img_name},{result}"
-print(f"{save_name}")
+    model.eval()
+    img_tensor = transforms.ToTensor()(img).unsqueeze_(0)
+    outputs = model(img_tensor)
+    _, predicted = torch.max(outputs.data, 1)
+
+    with open("./imagenet-labels.json") as f:
+        labels = json.load(f)
+
+    result = labels[np.array(predicted)[0]]
+    img_name = image_location.split("/")[-1]
+    save_name = f"{img_name},{result}"
+
+    print(f"{save_name}")
+
+    return save_name
+
+
+if __name__ == "__main__":
+    url = str(sys.argv[1])
+    image_recognition(image_location=url, parent_folder="data")
