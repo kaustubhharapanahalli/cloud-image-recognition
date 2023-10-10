0eccc3feced062c09bff33262f366cb9e9f98968
Kaustubh Harapanahalli <kaustubhharapanahalli@gmail.com>
2023-10-10 01:08:15 -0700

    Code linting

--- controller/createEc2Instance.py
+++ controller/createEc2Instance.py
@@ -1,10 +1,11 @@
 import boto3
-ec2 = boto3.resource('ec2')
+
+ec2 = boto3.resource("ec2")
 
 instances = ec2.create_instances(
-        ImageId="ami-09c6ef0459a2ff40e",
-        MinCount=1,
-        MaxCount=1,
-        InstanceType="t2.micro"
-    )
+    ImageId="ami-09c6ef0459a2ff40e",
+    MinCount=1,
+    MaxCount=1,
+    InstanceType="t2.micro",
+)
 print(instances[0].id)
--- controller/testRecieve.py
+++ controller/testRecieve.py
@@ -1,30 +1,23 @@
 import boto3
 
 # Create SQS client
-sqs = boto3.client('sqs')
+sqs = boto3.client("sqs")
 
-queue_url = 'https://sqs.us-east-1.amazonaws.com/697190715831/myQueue.fifo'
+queue_url = "https://sqs.us-east-1.amazonaws.com/697190715831/myQueue.fifo"
 
 # Receive message from SQS queue
 response = sqs.receive_message(
     QueueUrl=queue_url,
-    AttributeNames=[
-        'SentTimestamp'
-    ],
+    AttributeNames=["SentTimestamp"],
     MaxNumberOfMessages=1,
-    MessageAttributeNames=[
-        'All'
-    ],
+    MessageAttributeNames=["All"],
     VisibilityTimeout=10,
-    WaitTimeSeconds=0
+    WaitTimeSeconds=0,
 )
 
-message = response['Messages'][0]
-receipt_handle = message['ReceiptHandle']
+message = response["Messages"][0]
+receipt_handle = message["ReceiptHandle"]
 
 # Delete received message from queue
-sqs.delete_message(
-    QueueUrl=queue_url,
-    ReceiptHandle=receipt_handle
-)
-print('Received and deleted message: %s' % message)
+sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
+print("Received and deleted message: %s" % message)
